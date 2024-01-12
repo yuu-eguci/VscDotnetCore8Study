8e7933cb3b5f67637d78f6b0bc450c2de4554131
yuu-eguci <yuu.eguci@gmail.com>
2024-01-12 09:34:23 +0900

    ✨ Console.ReadLine を使うと入力を受け付けることができる

--- Program.cs
+++ Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("What is your name?");
+        // この文字列を name という変数に格納
+        string? name = Console.ReadLine();
+        DateTime currentDate = DateTime.Now;
+        // NewLine は、プラットフォームに依存せず、言語に依存せずに、改行を表す方法です。 これは C# の \n と同じ
+        Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+        Console.Write($"{Environment.NewLine}Press any key to exit...");
+        // コンソール ウィンドウにプロンプトを表示し、Console.ReadKey(Boolean) メソッドを呼び出してユーザーによる入力を待つ
+        Console.ReadKey(true);
     }
 }
